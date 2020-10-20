3e248a459cfe6c6a08b73d1907afba53b7d769c1
mateus.borja <mateus.borja@gmail.com>
2020-10-20 17:00:32 -0400

    uri 1011

--- src/com/mateusborja/urionlinejava1/uri1011/uri1011.java
+++ src/com/mateusborja/urionlinejava1/uri1011/uri1011.java
@@ -1,3 +1,4 @@
+//java casting
 package com.mateusborja.urionlinejava1.uri1011;
 
 import java.util.Locale;
@@ -10,11 +11,12 @@ public class uri1011 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double raio, pi = 3.14159, volumeEsfera;
+        double raio, pi, volumeEsfera;
+        pi = 3.14159;
 
         raio = sc.nextDouble();
 
-        volumeEsfera = (4.0/3) * pi * raio * raio * raio;
+        volumeEsfera = (4.0 / 3) * pi * raio * raio * raio;
 
         System.out.printf("VOLUME = %.3f%n", volumeEsfera);
 
