6c865ebd8cbecd7574cdc0eb876150e819e1adfd
mateus.borja <mateus.borja@gmail.com>
2020-10-20 08:37:13 -0400

    uri example 1007

---
+++ src/com/mateusborja/urionlinejava1/uri1007/Main.java
@@ -0,0 +1,27 @@
+package com.mateusborja.urionlinejava1.uri1007;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int A, B, C, D, prod;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        D = sc.nextInt();
+
+        prod = (A * B - C * D);
+        System.out.println("DIFERENCA = " + prod);
+
+
+        sc.close();
+
+    }
+}
