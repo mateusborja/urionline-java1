d5c833b91ca0f52e837096555ce9bfc59b750df6
mateus.borja <mateus.borja@gmail.com>
2020-10-19 23:45:45 -0400

    uri example

--- src/com/mateusborja/urionlinejava1/uri1004/Main.java
+++ src/com/mateusborja/urionlinejava1/uri1004/Main.java
@@ -1,4 +1,26 @@
 package com.mateusborja.urionlinejava1.uri1004;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int x, y, prod;
+
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        prod = x * y;
+
+        System.out.println("PROD = " + prod);
+
+        sc.close();
+
+    }
+
 }
