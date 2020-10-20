3ab94fdb0597609305375c705be139be749d568a
mateus.borja <mateus.borja@gmail.com>
2020-10-19 23:44:44 -0400

    uri examples

--- src/com/mateusborja/urionlinejava1/uri1002/Main.java
+++ src/com/mateusborja/urionlinejava1/uri1002/Main.java
@@ -1,4 +1,25 @@
 package com.mateusborja.urionlinejava1.uri1002;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double A, R, pi = 3.14159;
+
+        R = sc.nextDouble();
+
+        A = pi * R * R;
+
+        System.out.printf("A=%.4f%n", A);
+
+        sc.close();
+
+    }
+
 }
