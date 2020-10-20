4bc5d71d0cdb54efa610626f342b069904c8bea9
mateus.borja <mateus.borja@gmail.com>
2020-10-20 11:08:30 -0400

    uri example 1012

---
+++ src/com/mateusborja/urionlinejava1/uri1013/Main.java
@@ -0,0 +1,30 @@
+//java uri 1013
+package com.mateusborja.urionlinejava1.uri1013;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c, maiorAB, maiorABC;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        maiorAB = (a + b + Math.abs(a - b)) / 2;
+
+        maiorABC = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+
+        System.out.println(maiorABC + " eh o maior");
+
+        sc.close();
+
+    }
+
+}
