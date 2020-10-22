d980a606b8806ee9202a8251ee578eaecf57c0f6
mateus.borja <mateus.borja@gmail.com>
2020-10-22 11:38:02 -0400

    uri 1020

---
+++ src/com/mateusborja/urionlinejava1/uri1020/Main.java
@@ -0,0 +1,34 @@
+package com.mateusborja.urionlinejava1.uri1020;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner entrada = new Scanner(System.in);
+
+        int n, ano, mes,  resto,  quociente;
+
+        n = entrada.nextInt();
+        resto = n;
+
+        ano = 365;
+        quociente = n / ano;
+        System.out.println(quociente + " ano(s)");
+        resto = resto % ano;
+
+        mes = 30;
+        quociente = resto / mes;
+        System.out.println(quociente + " mes(es)");
+        resto = resto % mes;
+
+        quociente = resto % 30;
+        System.out.println(quociente + " dia(s)");
+
+        entrada.close();
+
+    }
+}
