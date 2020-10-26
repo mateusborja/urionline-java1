d5ffdf2ac0c30fa14fbc6d29fb12b3a1400a435a
mateus.borja <mateus.borja@gmail.com>
2020-10-25 20:34:10 -0400

    uri 1113 resolved

---
+++ src/com/mateusborja/urionlinejava1/uri1113/Main.java
@@ -0,0 +1,28 @@
+//java while with if and else
+package com.mateusborja.urionlinejava1.uri1113;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        while (X!= Y) {
+            if (X < Y) {
+                System.out.println("Crescente");
+            } else {
+                System.out.println("Decrescente");
+            }
+
+            X = sc.nextInt();
+            Y = sc.nextInt();
+        }
+
+    }
+}
