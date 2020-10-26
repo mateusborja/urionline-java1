22f80bd0540e352a1dce0f3b8e06aa350a3d580b
mateus.borja <mateus.borja@gmail.com>
2020-10-26 07:54:23 -0400

    while example

---
+++ src/com/mateusborja/examplewhile/MediaIdades.java
@@ -0,0 +1,33 @@
+package com.mateusborja.examplewhile;
+
+import java.util.Scanner;
+
+public class MediaIdades {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int idade;
+
+        idade = sc.nextInt();
+        int soma = 0;
+        int ct = 0;
+
+        while(idade >=0) {
+
+            soma = soma + idade;
+            ct = ct + 1;
+            idade = sc.nextInt();
+        }
+
+        if (ct > 0) {
+            double  media = soma / ct;
+            System.out.printf("A media das idades e: %.2f%n", media);
+        } else {
+            System.out.println("Impossivel calcular.");
+        }
+
+        sc.close();
+    }
+}
