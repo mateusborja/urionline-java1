18c8c6afc5fae41824f6e465761569660d27ff87
mateus.borja <mateus.borja@gmail.com>
2020-10-27 10:16:47 -0400

    uri1038 com switch

--- src/com/mateusborja/urionlinejava1/uri1038/uri1038Switch.java
+++ src/com/mateusborja/urionlinejava1/uri1038/uri1038Switch.java
@@ -10,29 +10,38 @@ public class uri1038Switch {
 
         System.out.println("Digite o codigo do produto:");
         int codigo = sc.nextInt();
-
-        System.out.println("Digite a quantidade:");
-        int quantidade = sc.nextInt();
+        int quantidade;
 
         double total;
         switch (codigo) {
             case 1 -> {
+                System.out.println("Digite a quantidade:");
+                quantidade = sc.nextInt();
                 total = quantidade * 4.0;
                 System.out.printf("Total a pagar: %.2f%n", total);
             }
             case 2 -> {
+                System.out.println("Digite a quantidade:");
+                quantidade = sc.nextInt();
                 total = quantidade * 4.5;
                 System.out.printf("Total a pagar: %.2f%n", total);
             }
             case 3 -> {
+                System.out.println("Digite a quantidade:");
+                quantidade = sc.nextInt();
                 total = quantidade * 5.0;
                 System.out.printf("Total a pagar: %.2f%n", total);
             }
             case 4 -> {
+                System.out.println("Digite a quantidade:");
+                quantidade = sc.nextInt();
                 total = quantidade * 2.0;
                 System.out.printf("Total a pagar: %.2f%n", total);
             }
             case 5 -> {
+                System.out.println("Digite a quantidade:");
+                quantidade = sc.nextInt();
+
                 total = quantidade * 1.5;
                 System.out.printf("Total a pagar: %.2f%n", total);
             }
