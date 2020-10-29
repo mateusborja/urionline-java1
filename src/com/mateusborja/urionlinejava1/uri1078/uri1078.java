40c59a7850e0ef635003bcf38610df5e803f90a7
mateus.borja <mateus.borja@gmail.com>
2020-10-28 22:32:36 -0400

    uri 1078

---
+++ src/com/mateusborja/urionlinejava1/uri1078/uri1078.java
@@ -0,0 +1,23 @@
+//java for - uri 1078 - TABUADA
+package com.mateusborja.urionlinejava1.uri1078;
+
+import java.util.Scanner;
+
+public class uri1078 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 1; i <=10; i++) {
+            int produto = i * N;
+            System.out.println(i + " x " + N + " = " + produto);
+
+        }
+
+
+
+    }
+}
