c2b34ff0b38c0f9484d0e3fb1d6ff4efa6746256
mateus.borja <mateus.borja@gmail.com>
2020-10-30 15:25:37 -0400

    java array media, porcentagem

--- src/com/mateusborja/arrayexample01/ArrayStringIntDouble.java
+++ src/com/mateusborja/arrayexample01/ArrayStringIntDouble.java
@@ -29,9 +29,27 @@ public class ArrayStringIntDouble {
         System.out.println();
         for (int i = 0; i < n; i++) {
             System.out.printf("[Nome: %s], [Idade: %d anos], [Altura: %.2fm]%n", nome[i], idade[i], altura[i]);
-
         }
 
+        double soma = 0;
+        for (int i = 0; i < n; i++) {
+            soma += altura[i];
+        }
 
+        double mediaAltura = soma / n;
+
+        System.out.println();
+        System.out.printf("Media de Altura é: %.2fm%n", mediaAltura);
+
+        int ct = 0;
+        for (int i = 0; i < n; i++) {
+            if(idade[i] < 25)
+                ct++;
+        }
+
+        double porcentagem = ct * 100 / n;
+        System.out.printf("Porcentagem com menos de 25 anos: %.2f%%%n", porcentagem);
+
+        sc.close();
     }
 }
