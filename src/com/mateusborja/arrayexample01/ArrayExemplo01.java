8a20f593bfd3e2e18dee5b26d87774ef2f03d92c
mateus.borja <mateus.borja@gmail.com>
2020-10-30 12:23:19 -0400

    java array

---
+++ src/com/mateusborja/arrayexample01/ArrayExemplo01.java
@@ -0,0 +1,28 @@
+//java array
+package com.mateusborja.arrayexample01;
+
+import java.util.Scanner;
+
+public class ArrayExemplo01 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("qtde numeros na coleção: ");
+        int n = sc.nextInt();
+        int [] vet = new int[n];
+
+        System.out.print("digite os numeros da coleção: ");
+        for (int i = 0; i < n; i++) { //lendo array
+            vet[i] = sc.nextInt();
+        }
+
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(vet[i] + " ");
+
+        }
+
+    }
+}
