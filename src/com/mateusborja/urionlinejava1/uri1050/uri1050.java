ce58f02c567a7773252ffef4face8155feb40349
Mateus Borja <mateus.borja@gmail.com>
2020-12-16 22:32:19 -0400

    uri 1050

--- src/com/mateusborja/urionlinejava1/uri1050/uri1050.java
+++ src/com/mateusborja/urionlinejava1/uri1050/uri1050.java
@@ -18,36 +18,15 @@ public class uri1050 {
         int ddd = new Scanner(System.in).nextInt();
 
         switch (ddd) {
-
-            case 61:
-                System.out.println("Brasilia");
-                break;
-            case 71:
-                System.out.println("Salvador");
-                break;
-            case 11:
-                System.out.println("Sao Paulo");
-                break;
-            case 21:
-                System.out.println("Rio de Janeiro");
-                break;
-            case 32:
-                System.out.println("Juiz de Fora");
-                break;
-            case 19:
-                System.out.println("Campinas");
-                break;
-            case 27:
-                System.out.println("Vitoria");
-                break;
-            case 31:
-                System.out.println("Belo Horizonte");
-                break;
-
-            default:
-                System.out.println("DDD nao cadastrado");
-                break;
-
+            case 61 -> System.out.println("Brasilia");
+            case 71 -> System.out.println("Salvador");
+            case 11 -> System.out.println("Sao Paulo");
+            case 21 -> System.out.println("Rio de Janeiro");
+            case 32 -> System.out.println("Juiz de Fora");
+            case 19 -> System.out.println("Campinas");
+            case 27 -> System.out.println("Vitoria");
+            case 31 -> System.out.println("Belo Horizonte");
+            default -> System.out.println("DDD nao cadastrado");
 
         }
 
