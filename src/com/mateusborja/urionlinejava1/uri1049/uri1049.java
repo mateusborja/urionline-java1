4f6aeb5a432c1ac4d601c5dc25584b61319793f4
Mateus Borja <mateus.borja@gmail.com>
2020-12-16 21:33:39 -0400

    uri1049

---
+++ src/com/mateusborja/urionlinejava1/uri1049/uri1049.java
@@ -0,0 +1,68 @@
+/*
+ *
+ *  * Copyright (c) 2020.  | All rights reserved
+ *  * Mateus Borja // UX Designer • 3D Artist • Developer
+ *  * www.mateusborja.life
+ *
+ *
+ */
+
+package com.mateusborja.urionlinejava1.uri1049;
+
+import java.util.Scanner;
+
+public class uri1049 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String palavra1 = sc.nextLine();
+        String palavra2 = sc.nextLine();
+        String palavra3 = sc.nextLine();
+
+        if (palavra1.equals("vertebrado")) {
+
+            if (palavra2.equals("ave")) {
+
+                if (palavra3.equals("carnivoro")) {
+                    System.out.println("aguia");
+                } else {
+                    System.out.println("pomba");
+                }
+
+            } else {
+
+                if (palavra3.equals("onivoro")) {
+                    System.out.println("homem");
+                } else {
+                    System.out.println("vaca");
+
+                }
+            }
+        } else {
+
+            if (palavra2.equals("inseto")) {
+
+                if (palavra3.equals("hematofago")) {
+                    System.out.println("pulga");
+                } else {
+                    System.out.println("lagarta");
+
+                }
+            } else {
+
+                if (palavra3.equals("hematofago")) {
+                    System.out.println("sanguessuga");
+
+                } else {
+                    System.out.println("minhoca");
+
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
+
