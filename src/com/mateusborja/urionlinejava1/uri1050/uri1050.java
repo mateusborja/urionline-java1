a3546a0c2f02af968f21f1730dcd30a93fe624fd
Mateus Borja <mateus.borja@gmail.com>
2020-12-16 22:12:50 -0400

    uri 1050

---
+++ src/com/mateusborja/urionlinejava1/uri1050/uri1050.java
@@ -0,0 +1,55 @@
+/*
+ *
+ *  * Copyright (c) 2020.  | All rights reserved
+ *  * Mateus Borja // UX Designer • 3D Artist • Developer
+ *  * www.mateusborja.life
+ *
+ *
+ */
+
+package com.mateusborja.urionlinejava1.uri1050;
+
+import java.util.Scanner;
+
+public class uri1050 {
+
+    public static void main(String[] args) {
+
+        int ddd = new Scanner(System.in).nextInt();
+
+        switch (ddd) {
+
+            case 61:
+                System.out.println("Brasilia");
+                break;
+            case 71:
+                System.out.println("Salvador");
+                break;
+            case 11:
+                System.out.println("Sao Paulo");
+                break;
+            case 21:
+                System.out.println("Rio de Janeiro");
+                break;
+            case 32:
+                System.out.println("Juiz de Fora");
+                break;
+            case 19:
+                System.out.println("Campinas");
+                break;
+            case 27:
+                System.out.println("Vitoria");
+                break;
+            case 31:
+                System.out.println("Belo Horizonte");
+                break;
+
+            default:
+                System.out.println("DDD nao cadastrado");
+                break;
+
+
+        }
+
+    }
+}
