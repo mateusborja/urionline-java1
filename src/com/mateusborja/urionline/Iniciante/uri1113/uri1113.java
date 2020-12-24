c1ff769fcaa00b8324def2285bb4861f11acfff5
Mateus Borja <mateus.borja@gmail.com>
2020-12-24 17:56:08 -0400

    uri 1113

---
+++ src/com/mateusborja/urionline/Iniciante/uri1113/uri1113.java
@@ -0,0 +1,38 @@
+/*
+ *
+ *  * Copyright (c) 2020.  | All rights reserved
+ *  * Mateus Borja // UX Designer • 3D Artist • Developer
+ *  * www.mateusborja.life
+ *
+ *
+ */
+
+//java while with if and else
+package com.mateusborja.urionline.Iniciante.uri1113;
+
+import java.util.Scanner;
+
+public class uri1113 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        while (X!= Y) {
+            if (X < Y) {
+                System.out.println("Crescente");
+            } else {
+                System.out.println("Decrescente");
+            }
+
+            X = sc.nextInt();
+            Y = sc.nextInt();
+        }
+
+        sc.close();
+
+    }
+}
