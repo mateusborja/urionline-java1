7199edb9c69c40c6ec90c84edb4d3e8cf84c5ce2
Mateus Borja <mateus.borja@gmail.com>
2021-01-06 16:28:07 -0400

    uri 1015

--- src/com/mateusborja/urionline/Iniciante/uri1015/uri1015.java
+++ src/com/mateusborja/urionline/Iniciante/uri1015/uri1015.java
@@ -7,9 +7,47 @@
  *
  */
 
-/* java Math.sqrt(x) e Math.pow(x,y)
-distrancia entre dois pontos uri 1015
+
+/*
+
+Distância Entre Dois Pontos
+Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer
+no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas
+decimais após a vírgula, segundo a fórmula:
+
+distancia = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois
+valores de ponto flutuante: x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
+
+Saída
+Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+
+Exemplo de Entrada
+1.0 7.0
+5.0 9.0
+
+Exemplo de Saída
+4.4721
+
+Exemplo de Entrada
+-2.5 0.4
+12.1 7.3
+
+Exemplo de Saída
+16.1484
+
+Exemplo de Entrada
+2.5 -0.4
+-12.2 7.0
+
+Exemplo de Saída
+16.4575
+
  */
+
 package com.mateusborja.urionline.Iniciante.uri1015;
 
 import java.util.Locale;
