2301b8951ab63b1769030369f3f9224d6b18ebba
Mateus Borja <mateus.borja@gmail.com>
2021-01-06 16:39:06 -0400

    uri 1020

---
+++ src/com/mateusborja/urionline/Iniciante/uri1020/uri1020.java
@@ -0,0 +1,86 @@
+/*
+ *
+ *  * Copyright (c) 2020.  | All rights reserved
+ *  * Mateus Borja // UX Designer • 3D Artist • Developer
+ *  * www.mateusborja.life
+ *
+ *
+ */
+
+/*
+
+Idade em Dias
+Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a
+em anos, meses e dias
+
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo
+mês com 30 dias. Nos casos de teste nunca haverá uma situação que permite 12
+meses e alguns dias, como 360, 363 ou 364. Este é apenas um exercício com objetivo
+de testar raciocínio matemático simples.
+
+Entrada
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+Imprima a saída conforme exemplo fornecido.
+
+Exemplo de Entrada
+400
+
+Exemplo de Saída
+1 ano(s)
+1 mes(es)
+5 dia(s)
+
+Exemplo de Entrada
+800
+
+Exemplo de Saída
+2 ano(s)
+2 mes(es)
+10 dia(s)
+
+Exemplo de Entrada
+30
+
+Exemplo de Saída
+0 ano(s)
+1 mes(es)
+0 dia(s)
+
+ */
+
+package com.mateusborja.urionline.Iniciante.uri1020;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1020 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner entrada = new Scanner(System.in);
+
+        int n, ano, mes,  resto,  quociente;
+
+        n = entrada.nextInt();
+        resto = n;
+
+        ano = 365;
+        quociente = n / ano;
+        System.out.println(quociente + " ano(s)");
+        resto = resto % ano;
+
+        mes = 30;
+        quociente = resto / mes;
+        System.out.println(quociente + " mes(es)");
+        resto = resto % mes;
+
+        quociente = resto % 30;
+        System.out.println(quociente + " dia(s)");
+
+        entrada.close();
+
+    }
+}
