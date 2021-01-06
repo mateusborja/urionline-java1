30f4ac745188914af9c351b4ec91dd3f2f7c9218
Mateus Borja <mateus.borja@gmail.com>
2021-01-06 16:00:59 -0400

    uri 1002

---
+++ src/com/mateusborja/urionline/Iniciante/uri1002/uri1002.java
@@ -0,0 +1,70 @@
+/*
+ *
+ *  * Copyright (c) 2020.  | All rights reserved
+ *  * Mateus Borja // UX Designer • 3D Artist • Developer
+ *  * www.mateusborja.life
+ *
+ *
+ */
+
+/*
+
+Área do Círculo
+A fórmula para calcular a área de uma circunferência é: area = π . raio2.
+Considerando para este problema que π = 3.14159:
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+Entrada
+A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+
+Saída
+Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo
+abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double).
+Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado,
+caso contrário, você receberá "Presentation Error".
+
+Exemplos de Entrada
+2.00
+
+Exemplos de Saída
+A=12.5664
+
+Exemplos de Entrada
+100.64
+
+Exemplos de Saída
+A=31819.3103
+
+Exemplos de Entrada
+150.00
+
+Exemplos de Saída
+A=70685.7750
+
+ */
+
+package com.mateusborja.urionline.Iniciante.uri1002;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class uri1002 {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double A, R, pi = 3.14159;
+
+        R = sc.nextDouble();
+
+        A = pi * R * R;
+
+        System.out.printf("A=%.4f%n", A);
+
+        sc.close();
+
+    }
+
+}
