a678547d8a37fc7f1e9cba73530b9b8249def4d9
Mateus Borja <mateus.borja@gmail.com>
2021-01-06 16:36:33 -0400

    uri 1018

--- src/com/mateusborja/urionline/Iniciante/uri1018/uri1018.java
+++ src/com/mateusborja/urionline/Iniciante/uri1018/uri1018.java
@@ -1,5 +1,69 @@
-//java uri cedulas
-package com.mateusborja.urionlinejava1.uri1018;
+/*
+ *
+ *  * Copyright (c) 2020.  | All rights reserved
+ *  * Mateus Borja // UX Designer • 3D Artist • Developer
+ *  * www.mateusborja.life
+ *
+ *
+ */
+
+/*
+
+Cédulas
+Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis (cédulas) no qual o
+valor pode ser decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. A seguir mostre
+o valor lido e a relação de notas necessárias.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N (0 < N < 1000000).
+
+Saída
+Imprima o valor lido e, em seguida, a quantidade mínima de notas de cada tipo necessárias,
+conforme o exemplo fornecido. Não esqueça de imprimir o fim de linha após cada linha, caso
+contrário seu programa apresentará a mensagem: “Presentation Error”.
+
+Exemplo de Entrada
+576
+
+Exemplo de Saída
+576
+5 nota(s) de R$ 100,00
+1 nota(s) de R$ 50,00
+1 nota(s) de R$ 20,00
+0 nota(s) de R$ 10,00
+1 nota(s) de R$ 5,00
+0 nota(s) de R$ 2,00
+1 nota(s) de R$ 1,00
+
+Exemplo de Entrada
+11257
+
+Exemplo de Saída
+11257
+112 nota(s) de R$ 100,00
+1 nota(s) de R$ 50,00
+0 nota(s) de R$ 20,00
+0 nota(s) de R$ 10,00
+1 nota(s) de R$ 5,00
+1 nota(s) de R$ 2,00
+0 nota(s) de R$ 1,00
+
+Exemplo de Entrada
+503
+
+Exemplo de Saída
+503
+5 nota(s) de R$ 100,00
+0 nota(s) de R$ 50,00
+0 nota(s) de R$ 20,00
+0 nota(s) de R$ 10,00
+0 nota(s) de R$ 5,00
+1 nota(s) de R$ 2,00
+1 nota(s) de R$ 1,00
+
+ */
+
+package com.mateusborja.urionline.Iniciante.uri1018;
 
 import java.util.Locale;
 import java.util.Scanner;
