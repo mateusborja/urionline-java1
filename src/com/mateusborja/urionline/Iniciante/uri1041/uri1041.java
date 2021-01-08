9d974dae8a46e36536215339bf7e737c97772028
Mateus Borja <mateus.borja@gmail.com>
2021-01-08 18:14:43 -0400

    uri 1041

--- src/com/mateusborja/urionline/Iniciante/uri1041/uri1041.java
+++ src/com/mateusborja/urionline/Iniciante/uri1041/uri1041.java
@@ -7,7 +7,52 @@
  *
  */
 
-package com.mateusborja.urionlinejava1.uri1041;
+
+/*
+
+Coordenadas de um Ponto
+Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto
+em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre
+um dos eixos cartesianos ou na origem (x = y = 0).
+
+y
+q2
+q3
+x
+q1
+q4
+
+Se o ponto estiver na origem, escreva a mensagem “Origem”.
+
+Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+
+Entrada
+A entrada contem as coordenadas de um ponto.
+
+Saída
+A saída deve apresentar o quadrante em que o ponto se encontra.
+
+Exemplo de Entrada
+4.5 -2.2
+
+Exemplo de Saída
+Q4
+
+Exemplo de Entrada
+0.1 0.1
+
+Exemplo de Saída
+Q1
+
+Exemplo de Entrada
+0.0 0.0
+
+Exemplo de Saída
+Origem
+
+ */
+
+package com.mateusborja.urionline.Iniciante.uri1041;
 
 
 import java.util.Locale;
