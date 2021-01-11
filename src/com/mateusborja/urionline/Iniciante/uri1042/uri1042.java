108112940125c74e23fbb0260e2d16bf75af35e1
Mateus Borja <mateus.borja@gmail.com>
2021-01-11 08:18:59 -0400

    uri 1042

--- src/com/mateusborja/urionline/Iniciante/uri1042/uri1042.java
+++ src/com/mateusborja/urionline/Iniciante/uri1042/uri1042.java
@@ -7,7 +7,46 @@
  *
  */
 
-package com.mateusborja.urionlinejava1.uri1042;
+
+/*
+
+Sort Simples
+Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em
+ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos.
+
+Entrada
+A entrada contem três números inteiros.
+
+Saída
+Imprima a saída conforme foi especificado.
+
+Exemplo de Entrada
+7 21 -14
+
+Exemplo de Saída
+-14
+7
+21
+
+7
+21
+-14
+
+Exemplo de Entrada
+-14 21 7
+
+Exemplo de Saída
+-14
+7
+21
+
+-14
+21
+7
+
+ */
+
+package com.mateusborja.urionline.Iniciante.uri1042;
 
 import java.util.Locale;
 import java.util.Scanner;
